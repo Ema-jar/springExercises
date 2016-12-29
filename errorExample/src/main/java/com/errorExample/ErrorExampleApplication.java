757adee5cb20e2d2fb3a405e1602ca5009a0e81d
Emanuele Giarlini <emanuele.giarlini@gmail.com>
2016-12-29 19:37:27 +0100

    Error Example

---
+++ errorExample/src/main/java/com/errorExample/ErrorExampleApplication.java
@@ -0,0 +1,12 @@
+package com.errorExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorExampleApplication.class, args);
+	}
+}
