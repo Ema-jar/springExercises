d34110b567bf2cc843d7ebae19e5ca7e524b19ce
Emanuele Giarlini <emanuele.giarlini@gmail.com>
2016-12-30 17:11:03 +0100

    Database Example - configuration for H2

---
+++ DBExample/src/main/java/com/DBExample/DbExampleApplication.java
@@ -0,0 +1,12 @@
+package com.DBExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbExampleApplication.class, args);
+	}
+}
