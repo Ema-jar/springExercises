9ab72098fd9c141b6d48224c0e4a90845a379a7b
Ema-jar <emanuele.giarlini@gmail.com>
2017-03-17 02:23:27 +0100

    Repository creation

---
+++ h2demo/src/main/java/com/emajar/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.emajar.repository;
+
+import com.emajar.domain.Post;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by drago on 17/03/17.
+ */
+public interface PostRepository extends CrudRepository<Post, Long>{
+
+
+}
