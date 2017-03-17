525bc4c5707b83689969dcc9636a8ef891db4268
Ema-jar <emanuele.giarlini@gmail.com>
2017-03-17 01:25:08 +0100

    Entity creation

---
+++ h2demo/src/main/java/com/emajar/domain/Post.java
@@ -0,0 +1,68 @@
+package com.emajar.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+/**
+ * Created by drago on 17/03/17.
+ */
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String body;
+    private Date postedOn;
+
+    /**
+     * This annotation means that many posts can be created by the same author.
+     */
+    @ManyToOne
+    private Author author;
+
+
+    private Post(){
+        // for JPA
+    }
+
+    public Post(String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getPostedOn() {
+        return postedOn;
+    }
+
+    public void setPostedOn(Date postedOn) {
+        this.postedOn = postedOn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
