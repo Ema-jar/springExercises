e9648a64dd95828fd2c1ed1fb7051d2ec3b70b4c
Ema-jar <emanuele.giarlini@gmail.com>
2017-07-09 10:09:53 +0200

    statuc thymeleaf example

--- StaticPage/src/main/java/com/example/demo/StaticpageApplication.java
+++ StaticPage/src/main/java/com/example/demo/StaticpageApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StaticcontentApplication {
+public class StaticpageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StaticcontentApplication.class, args);
+		SpringApplication.run(StaticpageApplication.class, args);
 	}
 }
